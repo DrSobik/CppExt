54e8738ead39512cb7ccc3e4be57322afa7a19d4
DrSobik <DrSobik@localhost>
2016-03-20 12:06:55 +0100

    Int data types

--- MathExt/MathExt.h
+++ MathExt/MathExt.h
@@ -6,11 +6,13 @@
  */
 
 #ifndef MATHEXT_H
-#define	MATHEXT_H
+#define MATHEXT_H
 
 #include <cmath>
 #include <vector>
+#include <set>
 #include <algorithm>
+#include <cstdint>
 
 #include "Exceptions"
 
@@ -24,13 +26,33 @@ namespace Common {
 
 		/**********************************************************************/
 
-		typedef short int int16; // 16 bit
-		typedef int int32; // 32 bit
-		typedef long long int int64; // 64 bit
+		typedef int8_t int8; // 8 bit
+		typedef int16_t int16; // 16 bit
+		typedef int32_t int32; // 32 bit
+		typedef int64_t int64; // 64 bit
 
-		typedef unsigned short uint16; // 16 bit
-		typedef unsigned int uint32; // 32 bit
-		typedef unsigned long long int uint64; // 64 bit
+		typedef uint8_t uint8; // 8 bit
+		typedef uint16_t uint16; // 16 bit
+		typedef uint32_t uint32; // 32 bit
+		typedef uint64_t uint64; // 64 bit
+
+#ifdef MATHEXT_INTUNI_8
+		typedef Math::int8 intUNI;
+		typedef Math::uint8 uintUNI;
+#elif MATHEXT_INTUNI_16
+		typedef Math::int16 intUNI;
+		typedef Math::uint16 uintUNI;
+#elif MATHEXT_INTUNI_32
+		typedef Math::int32 intUNI;
+		typedef Math::uint32 uintUNI;
+#elif MATHEXT_INTUNI_64
+		typedef Math::int64 intUNI;
+		typedef Math::uint64 uintUNI;
+#else 
+		typedef Math::int64 intUNI;
+		typedef Math::uint64 uintUNI;
+#endif
+
 
 		/**********************************************************************/
 
@@ -43,19 +65,54 @@ namespace Common {
 		const double MIN_DOUBLE = -1e300;
 		const double EPSILON = 1e-15;
 
+		const int8 MAX_INT8 = int8((uint8(1) << 7) - 1); // 127
+		const int8 MIN_INT8 = ~MAX_INT8; // -128
 		const int16 MAX_INT16 = int16((uint16(1) << 15) - 1); // 32767
 		const int16 MIN_INT16 = ~MAX_INT16; // -32768
-		const int32 MAX_INT = int32((uint32(1) << 31) - 1); // 2147483647
-		const int32 MIN_INT = ~MAX_INT; // -2147483648
+		//		const int32 MAX_INT = int32((uint32(1) << 31) - 1); // 2147483647
+		//		const int32 MIN_INT = ~MAX_INT; // -2147483648
+		const int32 MAX_INT32 = int32((uint32(1) << 31) - 1); // 2147483647
+		const int32 MIN_INT32 = ~MAX_INT32; // -2147483648
 		const int64 MAX_INT64 = int64((uint64(1) << 63) - 1); // 9223372036854775807
 		const int64 MIN_INT64 = ~MAX_INT64; // -9223372036854775808
 
+		const uint16 MAX_UINT8 = uint8((uint16(1) << 8) - 1); // 255
+		const uint16 MIN_UINT8 = 0;
 		const uint16 MAX_UINT16 = uint16((uint32(1) << 16) - 1); // 65535
 		const uint16 MIN_UINT16 = 0;
-		const uint32 MAX_UINT = uint32((uint64(1) << 32) - 1); // 4294967295
-		const uint32 MIN_UINT = 0;
+		//		const uint32 MAX_UINT = uint32((uint64(1) << 32) - 1); // 4294967295
+		//		const uint32 MIN_UINT = 0;
+		const uint32 MAX_UINT32 = uint32((uint64(1) << 32) - 1); // 4294967295
+		const uint32 MIN_UINT32 = 0;
 		const uint64 MAX_UINT64 = uint64(((uint64(1) << 63) - 1) << 1) + 1; // 18446744073709551615
 		const uint64 MIN_UINT64 = 0;
+
+#ifdef MATHEXT_INTUNI_8
+		const intUNI MAX_INTUNI = MAX_INT8;
+		const intUNI MIN_INTUNI = MIN_INT8;
+		const uintUNI MAX_UINTUNI = MAX_UINT8;
+		const uintUNI MIN_UINTUNI = MIN_UINT8;
+#elif MATHEXT_INTUNI_16
+		const intUNI MAX_INTUNI = MAX_INT16;
+		const intUNI MIN_INTUNI = MIN_INT16;
+		const uintUNI MAX_UINTUNI = MAX_UINT16;
+		const uintUNI MIN_UINTUNI = MIN_UINT16;
+#elif MATHEXT_INTUNI_32
+		const intUNI MAX_INTUNI = MAX_INT32;
+		const intUNI MIN_INTUNI = MIN_INT32;
+		const uintUNI MAX_UINTUNI = MAX_UINT32;
+		const uintUNI MIN_UINTUNI = MIN_UINT32;
+#elif MATHEXT_INTUNI_64
+		const intUNI MAX_INTUNI = MAX_INT64;
+		const intUNI MIN_INTUNI = MIN_INT64;
+		const uintUNI MAX_UINTUNI = MAX_UINT64;
+		const uintUNI MIN_UINTUNI = MIN_UINT64;
+#else
+		const intUNI MAX_INTUNI = MAX_INT64;
+		const intUNI MIN_INTUNI = MIN_INT64;
+		const uintUNI MAX_UINTUNI = MAX_UINT64;
+		const uintUNI MIN_UINTUNI = MIN_UINT64;
+#endif  
 
 		/**********************************************************************/
 
@@ -63,7 +120,7 @@ namespace Common {
 		template<class T> void swap(T& x, T& y);
 
 		double pow(const double& x, const double& p);
-		double pow(const double& x, const int& p);
+		double pow(const double& x, const Math::intUNI& p);
 		double sqr(const double& x);
 		double sqrt(const double& x);
 		double exp(const double& x);
@@ -82,20 +139,20 @@ namespace Common {
 		double arctan(const double& x);
 		double arccot(const double& x);
 
-		int iabs(const int& x);
+		Math::intUNI iabs(const Math::intUNI& x);
 		double fabs(const double& x);
-		int abs(const int& x);
+		Math::intUNI abs(const Math::intUNI& x);
 		double abs(const double& x);
 
-		int cmp(const double& x1, const double& x2, const double& eps = Common::Math::EPSILON);
+		Math::intUNI cmp(const double& x1, const double& x2, const double& eps = Common::Math::EPSILON);
 
 		template<class T> const T& min(const T& x1, const T& x2);
 		template <template<class, class...> class V, class T, class... otherT> const T& min(const V<T, otherT...>& v);
-		template <template<class, class...> class V, class T, class... otherT> int minIdx(const V<T, otherT...>& v);
+		template <template<class, class...> class V, class T, class... otherT> Math::intUNI minIdx(const V<T, otherT...>& v);
 
 		template<class T> const T& max(const T& x1, const T& x2);
 		template <template<class, class...> class V, class T, class... otherT> const T& max(const V<T, otherT...>& v);
-		template <template<class, class...> class V, class T, class... otherT> int maxIdx(const V<T, otherT...>& v);
+		template <template<class, class...> class V, class T, class... otherT> Math::intUNI maxIdx(const V<T, otherT...>& v);
 
 		template <template<class, class...> class V, class T, class... otherT> T& sum(V<T, otherT...>& v);
 
@@ -104,13 +161,15 @@ namespace Common {
 
 		template <template<class, class...> class V, class T, class... otherT> void reverseVec(V<T, otherT...>& v);
 
+		template <template<class, class...> class V, class T, class... otherT> void removeDuplicates(V<T, otherT...>& v);
+
 		double round(const double& x);
 		double ceil(const double& x);
 		double floor(const double& x);
 
 		template <template<class, class...> class V, class T, class... otherT> bool nextLexPermutation(V<T, otherT...>& array);
 
-		unsigned long long int factorial(const int& n);
+		Math::uint64 factorial(const Math::uintUNI& n);
 		/**********************************************************************/
 
 		/**********************************************************************/
@@ -133,7 +192,7 @@ namespace Common {
 			return std::pow(x, p);
 		}
 
-		inline double pow(const double& x, const int& p) {
+		inline double pow(const double& x, const Math::intUNI& p) {
 			return std::pow(x, p);
 		}
 
@@ -143,7 +202,7 @@ namespace Common {
 
 		inline double sqrt(const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if (x < 0.0) throw ErrMsgException<Message<string>>(string("Common::MathExt::sqrt(const double x) negative argument!"));
+			if (x < 0.0) throw ErrMsgException<Message < string >> (string("Common::MathExt::sqrt(const double x) negative argument!"));
 #endif
 			return std::sqrt(x);
 		}
@@ -158,22 +217,22 @@ namespace Common {
 
 		inline double lg(const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::lg(const double x) small/negative argument!"));
+			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::lg(const double x) small/negative argument!"));
 #endif
 			return std::log10(x);
 		}
 
 		inline double ln(const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::ln(const double x) small/negative argument!"));
+			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::ln(const double x) small/negative argument!"));
 #endif
 			return std::log(x);
 		}
 
 		inline double log(const double& base, const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::lg(const double x) small/negative argument!"));
-			if (cmp(base, 0.0, EPSILON) <= 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::lg(const double x) small/negative base!"));
+			if (cmp(x, 0.0, EPSILON) <= 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::lg(const double x) small/negative argument!"));
+			if (cmp(base, 0.0, EPSILON) <= 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::lg(const double x) small/negative base!"));
 #endif
 			return ln(x) / ln(base);
 		}
@@ -192,15 +251,15 @@ namespace Common {
 
 		inline double tan(const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if ((cmp(round(x / Pi_2), x / Pi_2, EPSILON) == 0) && (cmp(round(x / Pi), x / Pi, EPSILON) != 0)) throw ErrMsgException<Message<string>>(string("Common::MathExt::tan(const double x) argument is multiplicity of Pi/2 !"));
+			if ((cmp(round(x / Pi_2), x / Pi_2, EPSILON) == 0) && (cmp(round(x / Pi), x / Pi, EPSILON) != 0)) throw ErrMsgException<Message < string >> (string("Common::MathExt::tan(const double x) argument is multiplicity of Pi/2 !"));
 #endif
 			return std::tan(x);
 		}
 
 		inline double cot(const double& x) {
 #ifndef MATHEXT_NO_CHECKS
-			if (cmp(round(x / Pi), x / Pi, EPSILON) == 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::cot(const double x) argument is multiplicity of Pi !"));
-			if (cmp(round(x / Pi_2), x / Pi_2, EPSILON) == 0) throw ErrMsgException<Message<string>>(string("Common::MathExt::cot(const double x) argument is multiplicity of Pi/2 !"));
+			if (cmp(round(x / Pi), x / Pi, EPSILON) == 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::cot(const double x) argument is multiplicity of Pi !"));
+			if (cmp(round(x / Pi_2), x / Pi_2, EPSILON) == 0) throw ErrMsgException<Message < string >> (string("Common::MathExt::cot(const double x) argument is multiplicity of Pi/2 !"));
 #endif
 			return 1.0 / tan(x);
 		}
@@ -226,7 +285,7 @@ namespace Common {
 		/**********************************************************************/
 
 		/** Integer abs */
-		inline int iabs(const int& x) {
+		inline Math::intUNI iabs(const Math::intUNI& x) {
 			return Common::Math::abs(x);
 		}
 
@@ -236,7 +295,7 @@ namespace Common {
 		}
 
 		/** Integer abs */
-		inline int abs(const int& x) {
+		inline Math::intUNI abs(const Math::intUNI& x) {
 			return (x < 0) ? -x : x;
 		}
 
@@ -248,7 +307,7 @@ namespace Common {
 
 		/**********************************************************************/
 
-		inline int cmp(const double& x1, const double& x2, const double& eps) {
+		inline Math::intUNI cmp(const double& x1, const double& x2, const double& eps) {
 			if (Common::Math::abs(x1 - x2) < eps) {
 				return 0;
 			} else {
@@ -259,7 +318,7 @@ namespace Common {
 				}
 			}
 
-			throw ErrMsgException<Message<string>>(string("Common::MathExt::cmp(const double x1, const double x2, const double eps) failed to compare values!"));
+			throw ErrMsgException<Message < string >> (string("Common::MathExt::cmp(const double x1, const double x2, const double eps) failed to compare values!"));
 		}
 
 		template<class T> inline const T& min(const T& x1, const T& x2) {
@@ -268,7 +327,7 @@ namespace Common {
 
 		template <template<class, class...> class V, class T, class... otherT> inline const T& min(const V<T, otherT...>& v) {
 			/*
-						int n = v.size();
+						Math::intUNI n = v.size();
 			
 			#ifndef MATHEXT_NO_CHECKS
 			
@@ -282,9 +341,9 @@ namespace Common {
 							return v[0];
 						}
 
-						int pos = 0;
+						Math::intUNI pos = 0;
 
-						for (int i = 1; i < n; i++) {
+						for (Math::intUNI i = 1; i < n; i++) {
 							if (v[pos] > v[i]) {
 								pos = i;
 							}
@@ -292,19 +351,19 @@ namespace Common {
 
 						return v[pos];
 			 */
-			
-			int idx = minIdx(v); 
-			
+
+			Math::intUNI idx = minIdx(v);
+
 			return v[idx];
 		}
 
-		template <template<class, class...> class V, class T, class... otherT> inline int minIdx(const V<T, otherT...>& v) {
-			int n = v.size();
+		template <template<class, class...> class V, class T, class... otherT> inline Math::intUNI minIdx(const V<T, otherT...>& v) {
+			Math::intUNI n = (Math::intUNI) v.size();
 
 #ifndef MATHEXT_NO_CHECKS
 
 			if (n == 0) {
-				throw ErrMsgException<Message<string>>(string("Common::MathExt::minIdx(const V<T> &v) the input vector is empty!"));
+				throw ErrMsgException<Message < string >> (string("Common::MathExt::minIdx(const V<T> &v) the input vector is empty!"));
 				return -1;
 			}
 #endif
@@ -313,9 +372,9 @@ namespace Common {
 				return 0;
 			}
 
-			int pos = 0;
+			Math::intUNI pos = 0;
 
-			for (int i = 1; i < n; i++) {
+			for (Math::intUNI i = 1; i < n; i++) {
 				if (v[pos] > v[i]) {
 					pos = i;
 				}
@@ -331,7 +390,7 @@ namespace Common {
 		template <template<class, class...> class V, class T, class... otherT> const T& max(const V<T, otherT...>& v) {
 			/*
 			
-				int n = v.size();
+				Math::intUNI n = v.size();
 
 	#ifndef MATHEXT_NO_CHECKS			
 
@@ -345,9 +404,9 @@ namespace Common {
 					return v[0];
 				}
 
-				int pos = 0;
+				Math::intUNI pos = 0;
 
-				for (int i = 1; i < n; i++) {
+				for (Math::intUNI i = 1; i < n; i++) {
 					if (v[pos] < v[i]) {
 						pos = i;
 					}
@@ -355,28 +414,28 @@ namespace Common {
 
 				return v[pos];
 			 */
-			
-			int idx = maxIdx(v); 
-			
+
+			Math::intUNI idx = maxIdx(v);
+
 			return v[idx];
 		}
 
-		template <template<class, class...> class V, class T, class... otherT> int maxIdx(const V<T, otherT...>& v) {
-			int n = v.size();
+		template <template<class, class...> class V, class T, class... otherT> Math::intUNI maxIdx(const V<T, otherT...>& v) {
+			Math::intUNI n = (Math::intUNI) v.size();
 #ifndef MATHEXT_NO_CHECKS
 			if (n == 0) {
-				throw ErrMsgException<Message<string>>(string("Common::MathExt::max(const V<T> &v) the input vector is empty!"));
+				throw ErrMsgException<Message < string >> (string("Common::MathExt::max(const V<T> &v) the input vector is empty!"));
 				return -1;
 			}
-#endif			
+#endif   
 
 			if (n == 1) {
 				return 0;
 			}
 
-			int pos = 0;
+			Math::intUNI pos = 0;
 
-			for (int i = 1; i < n; i++) {
+			for (Math::intUNI i = 1; i < n; i++) {
 				if (v[pos] < v[i]) {
 					pos = i;
 				}
@@ -390,20 +449,20 @@ namespace Common {
 		/**********************************************************************/
 
 		template <template<class, class...> class V, class T, class... otherT> T& sum(V<T, otherT...>& v) {
-			int n = v.size();
+			Math::intUNI n = (Math::intUNI) v.size();
 
-#ifndef MATHEXT_NO_CHECKS			
+#ifndef MATHEXT_NO_CHECKS   
 
 			if (n == 0) {
-				throw ErrMsgException<Message<string>>(string("Common::MathExt::sum(V<T>& v) : Summing an empty vector!!!"));
+				throw ErrMsgException<Message < string >> (string("Common::MathExt::sum(V<T>& v) : Summing an empty vector!!!"));
 				return T();
 			}
 
-#endif			
+#endif   
 
 			T res = v[0];
 
-			for (int i = 1; i < n; i++) {
+			for (Math::intUNI i = 1; i < n; i++) {
 				res = res + v[i];
 			}
 
@@ -439,11 +498,43 @@ namespace Common {
 		/**********************************************************************/
 
 		template <template<class, class...> class V, class T, class... otherT> void reverseVec(V<T, otherT...>& v) {
-			int n = v.size();
+			Math::intUNI n = (Math::intUNI) v.size();
 
-			for (int i = 0; i < n / 2; i++) {
+			for (Math::intUNI i = 0; i < n / 2; i++) {
 				Math::swap(v[i], v[n - 1 - i]);
 			}
+
+		}
+
+		/**********************************************************************/
+
+		/**********************************************************************/
+
+		template <template<class, class...> class V, class T, class... otherT> void removeDuplicates(V<T, otherT...>& v) {
+			Math::intUNI n = (Math::intUNI) v.size();
+			V<T, otherT...> newV;
+			std::set<Math::intUNI> posDel; // Positions to delete
+			Math::intUNI lastPos;
+
+			for (Math::intUNI i = 0; i < n - 1; i++) {
+				for (Math::intUNI j = i + 1; j < n; j++) {
+					if (v[i] == v[j]) {
+						posDel.insert(j);
+					}
+				}
+			}
+
+			lastPos = 0;
+			newV.resize(n - Math::intUNI(posDel.size()));
+			for (Math::intUNI i = 0; i < n; i++) {
+				if (posDel.find(i) == posDel.end()) { // Should not be removed
+					newV[lastPos] = v[i];
+					lastPos++;
+				}
+			}
+
+			// Remove the last elements
+			v = newV;
 
 		}
 
@@ -469,9 +560,9 @@ namespace Common {
 
 		/*
 		template <template<class,class> class V, class VA, class T> inline void randPermut(V<T,VA>& permut) {
-			int n = permut.size();
-			int i;
-			int j;
+			Math::intUNI n = permut.size();
+			Math::intUNI i;
+			Math::intUNI j;
 
 			for (i = 1; i < n; i++) {
 				j = qrand() % (i + 1);
@@ -481,9 +572,9 @@ namespace Common {
 		 */
 
 		//template <class T> void randPermut(vector<T>& permut) {
-		//	int n = permut.size();
-		//	int i;
-		//	int j;
+		//	Math::intUNI n = permut.size();
+		//	Math::intUNI i;
+		//	Math::intUNI j;
 
 		//	for (i = 1; i < n; i++) {
 		//		j = qrand() % (i + 1);
@@ -493,8 +584,8 @@ namespace Common {
 
 		template <template<class, class...> class V, class T, class... otherT> inline bool nextLexPermutation(V<T, otherT...>& array) {
 			// Find the non-increasing suffix
-			int length = array.size();
-			int i = length - 1;
+			Math::intUNI length = array.size();
+			Math::intUNI i = length - 1;
 
 			while (i > 0 && array[i - 1] >= array[i]) {
 				i--;
@@ -505,7 +596,7 @@ namespace Common {
 			}
 
 			// Find the successor to the pivot
-			int j = length - 1;
+			Math::intUNI j = length - 1;
 
 			while (array[j] <= array[i - 1]) {
 				j--;
@@ -533,10 +624,10 @@ namespace Common {
 
 		/**********************************************************************/
 
-		inline unsigned long long int factorial(const int& n) {
-			unsigned long long int res = 1;
+		inline Math::uint64 factorial(const Math::uintUNI& n) {
+			Math::uint64 res = 1;
 
-			for (int i = 2; i <= n; i++) {
+			for (Math::uintUNI i = 2; i <= n; i++) {
 				res *= i;
 			}
 
@@ -549,5 +640,5 @@ namespace Common {
 
 }
 
-#endif	/* MATHEXT_H */
+#endif /* MATHEXT_H */
 
